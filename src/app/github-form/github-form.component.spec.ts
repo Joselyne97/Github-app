1ca04a648a788da106ce0236cd5a2b4e463c7a27
Joselyne97 <joselynejojo740@gmail.com>
2019-08-29 12:29:53 +0200

    I modified app component html

--- src/app/github-form/github-form.component.spec.ts
+++ src/app/github-form/github-form.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GitFormComponent } from './git-form.component';
+import { GithubFormComponent } from './github-form.component';
 
-describe('GitFormComponent', () => {
-  let component: GitFormComponent;
-  let fixture: ComponentFixture<GitFormComponent>;
+describe('GithubFormComponent', () => {
+  let component: GithubFormComponent;
+  let fixture: ComponentFixture<GithubFormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GitFormComponent ]
+      declarations: [ GithubFormComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GitFormComponent);
+    fixture = TestBed.createComponent(GithubFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
