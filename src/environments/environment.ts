a99da260be3bd7650b6513bb5422ec706349ddea
Joselyne97 <joselynejojo740@gmail.com>
2019-08-29 10:22:33 +0200

    I added the app key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apikey:"6b207c61fb1f7bb05e8cbe29ccb550"
 };
 
 /*
