8fb09931c75abee437f42a38aff4c6a74db72fa1
Joselyne97 <joselynejojo740@gmail.com>
2019-08-29 10:53:21 +0200

    I added the bootstrap

---
+++ src/app/git-form/git-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ga-git-form',
+  templateUrl: './git-form.component.html',
+  styleUrls: ['./git-form.component.css']
+})
+export class GitFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
